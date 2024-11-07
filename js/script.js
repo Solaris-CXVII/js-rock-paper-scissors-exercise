66396f014b11e49088d2050d601b86be45b607fe
Solaris-CXVII <aldencua2403@gmail.com>
2024-11-07 23:48:55 +0800

    save progress for rollback possibility

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 console.log("hello world");
 
+const humanScore = 0;
+const computerScore = 0;
+
 function getComputerChoice() {
   const choiceMax = 3;
   function computerChoice(choiceMax) {
@@ -8,22 +11,34 @@ function getComputerChoice() {
   const computerAns = computerChoice(choiceMax);
 
   if (computerAns == 0) {
-    console.log("Rock");
+    console.log("rock");
   } else if (computerAns == 1) {
-    console.log("Paper");
+    console.log("paper");
   } else if (computerAns == 2) {
-    console.log("Scissors");
+    console.log("scissors");
   }
 }
-
-getComputerChoice();
 
 function getHumanChoice() {
   const humanChoice = window.prompt("Rock, paper, or scissors?", "");
+  const humanAns = humanChoice.toLowerCase();
 
-  if (humanChoice == "rock") {
+  if (humanAns == "rock") {
     console.log("rock");
+  } else if (humanAns == "paper") {
+    console.log("paper");
+  } else if (humanAns == "scissors") {
+    console.log("scissors");
+  } else {
+    console.log("Invalid input, choose either rock, paper, or scissors");
   }
 }
 
-getHumanChoice();
+function playRound(humanAns, computerAns) {
+  if {}
+}
+
+const humanSelect = getHumanChoice();
+const computerSelect = getComputerChoice();
+
+playRound(humanSelect, computerSelect);
