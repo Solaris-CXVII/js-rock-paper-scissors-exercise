231217149f3e2ee862cb88899971a88dbe90667c
Solaris-CXVII <aldencua2403@gmail.com>
2024-11-11 22:37:19 +0800

    no loops

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log("hello world");
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   const choiceMax = 3;
@@ -12,10 +12,13 @@ function getComputerChoice() {
 
   if (computerAns == 0) {
     console.log("rock");
+    return "rock";
   } else if (computerAns == 1) {
     console.log("paper");
+    return "paper";
   } else if (computerAns == 2) {
     console.log("scissors");
+    return "scissors";
   }
 }
 
@@ -25,20 +28,47 @@ function getHumanChoice() {
 
   if (humanAns == "rock") {
     console.log("rock");
+    return humanAns;
   } else if (humanAns == "paper") {
     console.log("paper");
+    return humanAns;
   } else if (humanAns == "scissors") {
     console.log("scissors");
+    return humanAns;
   } else {
     console.log("Invalid input, choose either rock, paper, or scissors");
   }
 }
 
 function playRound(humanAns, computerAns) {
-  if {}
+  if (humanAns == computerAns) {
+    console.log("Tie");
+  } else if (
+    (humanAns === "rock" && computerAns === "scissors") ||
+    (humanAns === "paper" && computerAns === "rock") ||
+    (humanAns === "scissors" && computerAns === "paper")
+  ) {
+    console.log("You win!");
+    humanScore++;
+  } else {
+    console.log("You lose!");
+    computerScore++;
+  }
 }
 
-const humanSelect = getHumanChoice();
-const computerSelect = getComputerChoice();
+function playGame() {
+  const humanSelect = getHumanChoice();
+  const computerSelect = getComputerChoice();
 
-playRound(humanSelect, computerSelect);
+  playRound(humanSelect, computerSelect);
+
+  if (humanScore == 5) {
+    console.log("U won bruv");
+  } else if (computerScore == 5) {
+    console.log("big L bruv");
+  } else {
+    playGame();
+  }
+}
+
+playGame();
